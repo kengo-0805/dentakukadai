2f5bdb30f088dd85d699240009b60af3e98f0f37
kengo horii <s18c1105wl@s.chibakoudai.jp>
2018-10-16 09:39:38 +0900

    シンプルな四則演算

--- dentaku1.c
+++ dentaku1.c
@@ -3,28 +3,23 @@
 
 int main(void)
 {
-    double  a, b, ans;
-    char    op;
+    double  x, y, answer;
+    char    a;
 
     printf( "四則演算をします。\n" );
     while( 1 ) {
         printf( "Ready : " );
-        scanf( "%lf %c %lf", &a, &op, &b );
-        switch( op ) {
-        case '+': ans = a + b; break;
-        case '-': ans = a - b; break;
-        case '*': ans = a * b; break;
-        case '/': if( b == 0.0 ) {
-                     printf( "ゼロでの割算はできません)\n" );
-                     continue;
-                  }
-                  ans = a / b; break;
-        default:  printf( "演算記号が誤りです)\n" );
-                  continue;
+        scanf( "%lf %c %lf", &x, &a, &y );
+        switch( a ) {
+        case '+': answer = x + y; break;
+        case '-': answer = x - y; break;
+        case '*': answer = x * y; break;
+        case '/': answer = x / y; break;
+        default:  printf( "演算記号が誤りです\n" );
+                  break;
         }
-        printf( "= %g\n", ans );
-    }
-    printf( ".... Power OFF\n" );
+        printf( "= %g\n", answer );
+    }    
 }
 
    
