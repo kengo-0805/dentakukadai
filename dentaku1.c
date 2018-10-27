08a0e2cbc93b0d2766d2049a7a3f77b0d12a9f6b
kengo horii <s18c1105wl@s.chibakoudai.jp>
2018-10-27 17:19:50 +0900

    完成!

--- dentaku1.c
+++ dentaku1.c
@@ -1,31 +1,77 @@
 #include "stdio.h"
 #include "math.h"
-#include "ctype.h"
+
+#define PI 3.141592
+	double rad1, rad2;
+	double x, y, limit1, limit2, answer;
+	char a,b,c,s,t,e;
+	char dammy;
+
+ 
 
 int main(void)
 {
-    double  a, b, ans;
-    char    op;
+	printf("四則演算をします\n終了するときはeを入力してください\n");
+	
+	while(1){
+		printf( "Ready:" );
+		scanf("%c",&b);		//sin cos tan
+		if(b == 'e')break;
+		printf("%c",b);
+		scanf("%lf",&x);	//数字
+		scanf("%c",&a);		//+-*/
+		scanf("%c",&c);		//sin cos tan
+		scanf("%lf",&y);	//数字
+		scanf("%c",&dammy);	//Enter
 
-    printf( "加減乗除(+,-,*,/)ができます。指定例:2+5、終了時はq\n" );
-    while( 1 ) {
-        printf( "ready : " );
-        if( scanf( "%lf %c %lf", &a, &op, &b ) != 3 ) break;
-        switch( op ) {
-        case '+': ans = a + b; break;
-        case '-': ans = a - b; break;
-        case '*': ans = a * b; break;
-        case '/': if( b == 0.0 ) {
-                     printf( "Error!(ゼロでの割算はできません)\n" );
-                     continue;
-                  }
-                  ans = a / b; break;
-        default:  printf( "Error!(演算記号の指定が誤りです)\n" );
-                  continue;
+		if(b == s || c || t) rad1 = x * PI / 180;	//三角関数の時のみ実行
+		if(c == s || c || t) rad2 = y * PI / 180;	//三角関数の時のみ実行
+	switch(b){
+		case 's':
+			limit1 = sin(rad1); break;
+		case 'c':
+			limit1 = cos(rad1); break;
+		case 't':
+			limit1 = tan(rad1); break;
+		case 'l':
+			limit1 = log(x); break;
+		
+		default: limit1 = x; break;
+	}
+	
+
+	switch(c){
+		
+        	case 's':
+			limit2 = sin(rad2); break;
+        	case 'c':
+			limit2 = cos(rad2); break;
+        	case 't':
+			limit2 = tan(rad2); break;
+        	case 'l':
+			limit2 = log(y); break;
+
+		default: limit2 = y; break;
         }
-        printf( "= %g\n", ans );
-    }
-    printf( ".... Power OFF\n" );
-}
+	
+	
+	switch(a) {
+		case '+':
+			answer = limit1 + limit2; break;
+		case '-':
+			answer = limit1 - limit2; break;
+		case '*':
+			answer = limit1 * limit2; break;
+		case '/':
+			if(limit2 == 0){
+				printf("定義外です。");
+			}
+			answer = limit1 / limit2; break;
+		
+		default:  printf( "演算記号が誤りです\n"); break;  
+	}
+	
+	printf("=%lf\n",answer);
+	}	
 
-   
+}   
